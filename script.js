29b7319fc28b3621ca7da78536d00b9585c228b4
Jade-Evans <jpgabb@msn.com>
2025-06-12 15:22:26 +0100

    chore(pages): add blank JS file and placeholder topic HTML files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//Adding change to style when hoving over any button in class="indexTopicButton"//
+const indexTopicButton = document.getElementById("indexTopicButton");
+//const hoverIndexButtonStyle = function;//
+//Adding links to the topic pages - assigning INDEX BUTTON VARIABLES//
+const exploreFoodButton = document.getElementById("exploreFoodButton");
+const exploreArchitectureButton = document.getElementById("exploreArchitectureButton");
+const exploreCultureButton = document.getElementById("exploreCultureButton");
+const exploreGardenButton = document.getElementById("exploreGardenButton");
+const exploreEntertainmentButton = document.getElementById("exploreEntertainmentButton");
+
